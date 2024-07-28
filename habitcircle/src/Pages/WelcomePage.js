6eb57692d4c460a2f7ca15cd10e4d0c9b21b88f8
Teresa Lavoie <teresa.lavoie@ucalgary.ca>
2024-07-27 18:27:41 -0600

    welcome page just frontend (button doesn't work yet)

--- habitcircle/src/Pages/WelcomePage.js
+++ habitcircle/src/Pages/WelcomePage.js
@@ -1,16 +1,23 @@
-//welcome page
 import React from 'react';
 import './Styling/WelcomePage.css';
 
 function WelcomePage() {
-
     return (
         <>
-
-            <p>Welcome Page</p>
-        
+            <div className='sunrise-background'>
+                <div className="sun"></div>
+                <div className="welcome-content">
+                    <div className='top-welcome'>
+                        <p>welcome to</p>
+                        <h1>HabitCircle</h1>
+                    </div>
+                    <div className='bottom-welcome'>
+                        <button>Get Started</button>
+                    </div>
+                </div>
+            </div>
         </>
-    )
+    );
 }
 
 export default WelcomePage;
