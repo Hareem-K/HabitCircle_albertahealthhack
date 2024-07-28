ad5ea49c5aed4ba9a8428bc808521efd08224e10
Teresa Lavoie <teresa.lavoie@ucalgary.ca>
2024-07-27 19:47:04 -0600

    added user select page (buttons don't work yet)

--- habitcircle/src/Pages/UserSelect.js
+++ habitcircle/src/Pages/UserSelect.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Styling/UserSelect.css';
 
 function UserSelect() {
+    const navigate = useNavigate();
+
+    const handleReturningUserClick = () => {
+        navigate('/returning-user');
+    };
+
+    const handleNewUserClick = () => {
+        navigate('/new-user');
+    };
+
     return (
-        <div>
-            <h1>User Select Page</h1>
-            <p>This is the user selection page.</p>
+        <div className='user-select-background'>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="user-select-content">
+                <div className="user-select-buttons">
+                    <button onClick={handleReturningUserClick}>Returning User</button>
+                    <button onClick={handleNewUserClick}>New User</button>
+                </div>
+            </div>
         </div>
     );
 }
